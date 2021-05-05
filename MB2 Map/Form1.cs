5e457d25627abc3e614a70d62ef03546adbb2748
Kevinf100 <kevinf400@hotmail.com>
2021-05-04 20:33:34 -0400

    Add project files.

---
+++ MB2 Map/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MB2_Map
+{
+    public partial class Form1 : Form
+    {
+        private TownList _towns;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _towns = new TownList();
+            listBox1.Items.Clear();
+            var loadFilesThread = new Thread(Load_Files_Into_ListBox);
+            loadFilesThread.Start();
+        }
+        private void Load_Files_Into_ListBox()
+        {
+            var wd = @$"{Directory.GetCurrentDirectory()}\Towns";
+            var di = new DirectoryInfo(wd);
+            //Debugger.Break();
+            var files = di.GetFiles("*.txt");
+            foreach (var file in files)
+            {
+                if (file.DirectoryName == null)
+                    continue;
+                using var sr = new StreamReader(file.FullName);
+                var line = sr.ReadLine();
+                if (line == null)
+                    continue;
+                var splitLine = line.Split(",");
+                if (splitLine.Length != 2)
+                    continue;
+                Invoke(new MethodInvoker(delegate
+                {
+                    var extRemoved = file.Name.Remove(file.Name.Length - file.Extension.Length);
+                    _towns.AddTown(extRemoved, int.Parse(splitLine[0]), int.Parse(splitLine[1]));
+                    listBox1.Items.Add(extRemoved);
+                    listBox2.Items.Add(extRemoved);
+                }));
+            }
+            wd = @$"{Directory.GetCurrentDirectory()}\TrueDistance.txt";
+            using var sr2 = new StreamReader(wd);
+            while (!sr2.EndOfStream)
+            {
+                var line = sr2.ReadLine();
+                if (line == null)
+                    continue;
+                var splitLine = line.Split(",");
+                if (splitLine.Length != 3)
+                    continue;
+                Invoke(new MethodInvoker(delegate
+                {
+                    _towns.AddTrueLocation(splitLine[0], splitLine[1], uint.Parse(splitLine[2]));
+                }));
+            }
+
+            Invoke(new MethodInvoker(delegate
+            {
+                listBox1.Sorted = true;
+                listBox2.Sorted = true;
+                listBox1.SelectedIndex = 0;
+                listBox2.SelectedIndex = 1;
+                Enabled = true;
+            }));
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null || listBox2.SelectedItem == null)
+                return;
+            label1.Text = _towns.GetTownsDistance((string)listBox1.SelectedItem, (string)listBox2.SelectedItem).ToString();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null || listBox2.SelectedItem == null)
+                return;
+            label1.Text = _towns.GetTownsDistance((string)listBox1.SelectedItem, (string)listBox2.SelectedItem).ToString();
+        }
+    }
+}
---
+++ MB2 Map/TownList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MB2_Map
+{
+    class TownList
+    {
+        public class Town
+        {
+            public string Name { get; }
+            public Point Location { get; }
+
+            public Town(string name, Point location)
+            {
+                Name = name;
+                Location = location;
+            }
+            public Town(string name, int locationX, int locationY)
+            {
+                Name = name;
+                Location = new Point(locationX, locationY);
+            }
+
+        }
+        private readonly Dictionary<string, Town> _townDictionary = new();
+        private readonly Dictionary<(string, string), uint> _trueDistance = new();
+        public List<string> TownsList { get; } = new();
+        public void AddTown(string name, Point loc)
+        {
+            _townDictionary.Add(name, new Town(name, loc));
+            TownsList.Add(name);
+        }
+        public void AddTown(string name, int locationX, int locationY)
+        {
+            _townDictionary.Add(name, new Town(name, locationX, locationY));
+            TownsList.Add(name);
+        }
+
+        public void AddTrueLocation(string town1, string town2, uint distance)
+        {
+            string[] townArray = { town1, town2 };
+            Array.Sort(townArray);
+            _trueDistance.Add((townArray[0], townArray[1]), distance);
+        }
+
+        public Point GetTownPoint(string name)
+        {
+            try
+            {
+                return _townDictionary[name].Location;
+            }
+            catch (KeyNotFoundException)
+            {
+                return Point.Empty;
+            }
+        }
+        public uint GetTownsDistance(string town1, string town2)
+        {
+            try
+            {
+                //Debugger.Break();
+                string[] townArray = { town1, town2 };
+                Array.Sort(townArray);
+                return _trueDistance[(townArray[0], townArray[1])];
+            }
+            catch (KeyNotFoundException)
+            {
+                try
+                {
+                    var town1Obj = _townDictionary[town1];
+                    var town2Obj = _townDictionary[town2];
+                    return (uint)Math.Sqrt(Math.Pow(town2Obj.Location.X - town1Obj.Location.X, 2) + Math.Pow(town2Obj.Location.Y - town1Obj.Location.Y, 2));
+                }
+                catch (KeyNotFoundException)
+                {
+                    return default;
+                }
+            }
+        }
+    }
+}
